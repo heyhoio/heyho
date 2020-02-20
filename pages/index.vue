e571ded204415599a5e1c4a6995e01c2bad78887
Douglas Pires <douglas.pires@live.com>
2020-02-20 00:01:01 -0300

    create regex replace viewer

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,28 @@
 <template>
   <v-layout column justify-center align-center>
-    <h1>Here are some DevTools!</h1>
-    <p>As developers we care to provide awesome tooling!</p>
-    <nuxt-link v-for="({ to, text }, index) in links" :key="to" :to="to"
-      >{{ index + 1 }}.{{ text }}</nuxt-link
-    >
+    <h1 class="font-weight-thin">Here are some DevTools!</h1>
+    <p class="headline font-weight-light">
+      As developers we care to provide awesome tooling!
+    </p>
+    <v-list width="100%">
+      <nuxt-link
+        v-for="({ to, text }, index) in links"
+        :key="to"
+        :to="to"
+        tag="v-list-item"
+        >{{ index + 1 }}. {{ text }}</nuxt-link
+      >
+    </v-list>
   </v-layout>
 </template>
 
 <script>
 export default {
   data: () => ({
-    links: [{ to: '/color-picker', text: 'Color Picker' }]
+    links: [
+      { to: '/color-picker', text: 'Color Picker' },
+      { to: '/regex-replace-viewer', text: 'Regex Replace Viewer' }
+    ]
   })
 }
 </script>
---
+++ pages/regex-replace-viewer.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-layout column justify-center align-center>
+    <h1 class="font-weight-thin mb-5">Regex Replace Viewer</h1>
+    <v-flex>
+      <v-text-field v-model="regex" outlined label="RegEx" />
+      <v-text-field v-model="text" outlined label="Text" />
+      <v-text-field v-model="replaceBy" outlined label="Replace By" />
+    </v-flex>
+    <v-flex>
+      <p>Result:</p>
+      <p>{{ result }}</p>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'RegexViewer',
+  data: () => ({
+    regex: '\\.',
+    replaceBy: ' replaced string here ',
+    text: 'mywebsite.com'
+  }),
+  computed: {
+    result() {
+      let regex: RegExp | string = ''
+      try {
+        regex = new RegExp(`${this.regex}`, 'g')
+      } catch (error) {}
+      return String(this.text).replace(regex, this.replaceBy)
+    }
+  }
+})
+</script>
