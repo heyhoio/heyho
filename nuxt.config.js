757eaab40566f76df58de9b434eb484658c0d4cb
Douglas Pires <douglas.pires@live.com>
2020-02-29 18:15:11 -0300

    join strings

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    titleTemplate: '%s - ' + 'Heyho',
+    titleTemplate: '%s - Heyho',
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
