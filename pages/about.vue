65310d0a7ce4a13d06a6a5285126fd451f8b82d2
valentina <vaahxx@gmail.com>
2020-02-29 17:41:37 -0300

    add contributors' names

--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,7 @@
 <template>
   <v-layout column justify-center align-center>
     <h1 class="font-weight-thin">About us!</h1>
-    <p class="headline font-weight-light">
-      Some of us:
-    </p>
+    <p class="headline font-weight-light">Some of us:</p>
     <v-list width="100%">
       <v-list-item
         v-for="({ to, text }, index) in links"
@@ -22,7 +20,12 @@ import Vue from 'vue'
 export default Vue.extend({
   name: 'About',
   data: () => ({
-    links: [{ to: 'https://github.com/douglas-pires', text: 'Douglas Pires' }]
+    links: [
+      { to: 'https://github.com/douglas-pires', text: 'Douglas Pires' },
+      { to: 'https://github.com/AustinFelipe', text: 'Austin Felipe' },
+      { to: 'https://github.com/Carl13da', text: 'Carlos Henrique' },
+      { to: 'https://github.com/vaahxx', text: 'Valentina Garavaglia' }
+    ]
   }),
   head() {
     return {
