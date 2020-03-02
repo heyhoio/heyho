6c993d612781aa6875a52dbc17f456078f5d9605
Douglas Pires <douglas.pires@live.com>
2020-03-02 01:45:12 -0300

    linter

--- utils/base64.js
+++ utils/base64.js
@@ -1,2 +1,2 @@
-export const encode = (input) => Buffer.from(input).toString('base64')
-export const decode = (input) => Buffer.from(input, 'base64').toString('utf-8')
+export const encode = input => Buffer.from(input).toString('base64')
+export const decode = input => Buffer.from(input, 'base64').toString('utf-8')
